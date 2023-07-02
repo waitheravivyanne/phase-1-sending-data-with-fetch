68aa53aea083457b64c99ebbd0b48ee895060e2c
vivian waithera <vivyannekori@gmail.com>
2023-07-02 20:39:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+// let name = "Coco"
+// let email = "coco@prime.com"
+// let body = document.querySelector('body')
+
+
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+        .then(response => response.json())
+        .then(function (object) {
+            document.body.innerHTML = object['id']
+
+        })
+        .catch(function (error) {
+            document.body.innerHTML = error.message
+        })
+
+
+
+
+
+}
+
+
